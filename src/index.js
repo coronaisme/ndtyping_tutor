2b037a39cab18d67aea5cda3345406b3cedf612e
coronaisme <coronaisme@gmail.com>
2020-02-10 16:23:31 -0800

    added some containers

--- src/index.js
+++ src/index.js
@@ -8,15 +8,38 @@ function getUsername() {
   const userInput = document.getElementsByClassName('form-control')[0]
   const userSubmit = document.getElementsByClassName('btn float-right login_btn')[0]
   let userInputVal = userInput.value
-    
+  
   userSubmit.addEventListener("click", function(event) {
     event.preventDefault()   
+    renderGamePage()
     //once logged in, remember username, and render new HTML for actual game page
   })
 }
 
 function renderGamePage() {
-//page will include a side bar with
+  const mainContainer = document.getElementsByClassName('container')[0]
+  console.log(mainContainer)
+  mainContainer.innerHTML = `
+  <div class="title" <h1>ndTypingTutor</h1></div>
+  <div class="sidebar" id="sidebar">
+    <div class="username-tab"><p>Username</p>
+    </div><br>
+      <div class="user-scores-tab" id="user-scores"><a href="#user-scores">Username's High Scores</a>
+      </div><br>
+        <div class="leaderboard-tab" id="leaderboard"><a href="#leaderboard">Leaderboard</a>
+        </div><br>
+          <button class="start-button" id="start-btn">Start</button><br><br>
+            <div class="logout-tab" id="logout"><a href="#logout">Logout</a>
+    </div>
+  </div>
+  <div class="game-body" id="game-body">
+         
+  </div> 
+    <div class="bottom-bar" id="bot-bar"> 
+
+  </div>  `
+
+  //page will include a side bar with
   //username
     //button linking to users past games with scores(ordered by score)
     //button linking to the leaderboard (top ten highest scores of all users)
