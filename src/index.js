5bdffa7b98d6476be98e5611db400e596cb2a1ab
coronaisme <coronaisme@gmail.com>
2020-02-12 17:56:02 -0800

    merging

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
+let countDown;
 //page will include a side bar with
 //username
-  //button linking to users past games with scores(ordered by score)
-  //button linking to the leaderboard (top ten highest scores of all users)
-  //exit button - re render main page(logout)
+//button linking to users past games with scores(ordered by score)
+//button linking to the leaderboard (top ten highest scores of all users)
+//exit button - re render main page(logout)
 
 //main container where words will be falling
 //eventlistener to catch words being typed 
@@ -12,11 +13,11 @@
 
 
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
 
   listenForSignUp()
   listenForLogin()
-  
+
 })
 
 //if user hits sign up, makes a post, creates user
@@ -26,14 +27,15 @@ function listenForSignUp() {
     let userInput = document.getElementsByClassName('form-control')[0]
     event.preventDefault()
     fetch('http://localhost:3000/players', {
-      method: "POST", 
-      headers: {'Content-Type': 'application/json'},
+      method: "POST",
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
         name: userInput.value
       })
     }).then(res => res.json())
-    .then(data => {
-      renderGamePage(data)})
+      .then(data => {
+        renderGamePage(data)
+      })
   })
 }
 
@@ -42,24 +44,22 @@ function listenForLogin() {
   const loginBtn = document.getElementById('login-btn')
   let userInput = document.getElementsByClassName('form-control')[0]
 
-  loginBtn.addEventListener('click', function(event) {
+  loginBtn.addEventListener('click', function (event) {
     event.preventDefault()
     fetch('http://localhost:3000/players')
-    .then(res => res.json())
-    .then(data => data.forEach(obj => {
-      if(userInput.value === "") {
-        console.log("Invalid username")
-      }else if(obj.name === userInput.value){
-        renderGamePage(obj)
-      }
-    }))
+      .then(res => res.json())
+      .then(data => data.forEach(obj => {
+        if (userInput.value === "") {
+          console.log("Invalid username")
+        } else if (obj.name === userInput.value) {
+          renderGamePage(obj)
+        }
+      }))
   })
 }
 
 //change speed of interval
-function selectDifficulty() {
 
-}
 
 
 
@@ -73,110 +73,167 @@ function renderGamePage(user) {
   <div class="sidebar" id="sidebar">
     <div class="username-tab"><p>Hello, ${user.name}</p>
     </div><br>
-      <div class="user-scores-tab" id="user-scores"><a href="#user-scores">${user.name}'s Scores</a>
+      <div class="user-scores-tab" id="${user.id}"><a href="#user-scores">${user.name}'s Scores</a>
       </div><br>
         <div class="leaderboard-tab" id="leaderboard"><a href="#leaderboard">Leaderboard</a>
         </div><br>
           <button class="start-button" id="start-btn">Start</button><br><br>
+          <p>score:</p>
             <div class="score" id="score">0</div>
             <div class="logout-tab" id="logout"><a id="logout-link" href="#logout">Logout</a>
     </div>
+    <p>Timer: </p>
+    <div class="timer" id="timer">5</div>
   </div>
   <div class="game-body" id="game-body">
          <ul class "game-ul" id="game-ul">
          </ul>
-  </div> 
-    <div class="bottom-bar" id="bot-bar">
-     <form autocomplete="off" id="input-word">
-        <input id="text-input-id" type="text">
-     </form>
-     </div>
-  </div>  `
-  
- 
+         </div> 
+         <div class="bottom-bar" id="bot-bar">
+         <form autocomplete="off" id="input-word">
+         <input id="text-input-id" type="text">
+         </form>
+         </div>
+         </div>  `
 
-  
-listenForStartButton(user)
-listenLogout()
-listenForUserInput()
+
+
+
+  listenForStartButton(user)
+  listenLogout()
+  listenForUserInput()
 }
 
+
+function func() {
+  let timer = document.getElementById('timer')
+  let timerVal = parseInt(timer.innerText)
+  
+  if (timerVal > 0) {
+    timerVal--
+    timer.innerText = timerVal
+  } else {
+    timer.innerText = 0
+  }
+}
 //starts the interval to display words
-function listenForStartButton(user){
+
+function listenForStartButton(user) {
   const startButton = document.getElementById('start-btn');
-  startButton.addEventListener('click', function(event){
-    fetch('http://localhost:3000/words').then(resp => resp.json()).then(words => displayTheWords(words))
-    fetch('http://localhost:3000/games', {
-      method: "POST",
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({
-        player_id: user.id
-      })
-    }).then(resp => resp.json()).then(data => console.log(data))
+  let timer = document.getElementById('timer')
+  let timerVal = parseInt(timer.innerText)
+  
+  startButton.addEventListener('click', function (event) {
+    
+      setInterval(func, 1000)
+      
+      fetch('http://localhost:3000/words').then(resp => resp.json()).then(words => displayTheWords(words))
+      fetch('http://localhost:3000/games', {
+        method: "POST",
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          player_id: user.id
+        })
+      }).then(resp => resp.json()).then(gameData => console.log(gameData))
+    })
+  }
+  
+  
+  //gets words to add to a list at interval time, stops after x seconds
+  function displayTheWords(words) {
+    const scoreDiv = document.getElementById('score') //dont worry about it
+    const gameBodyUl = document.getElementById('game-ul')
+    // let list = gameBodyUl.children
+    // let arr = [...list]
+    let timer = document.getElementById('timer')
+    
+    countDown = setInterval(function () {
+      if (parseFloat(timer.innerText) > 0) {
+        const wordLi = document.createElement('li')
+        // console.log(parseFloat(timer.innerText))
+        //getting a random word obj
+        let randomWord = words[Math.floor(Math.random() * words.length)]
+        //setting our created wordLi id 
+        wordLi.id = randomWord.id
+        wordLi.value = randomWord.letter_count
+        //setting our created wordLi text
+        wordLi.innerText = randomWord.title
+        //append in an interval of 2.5seconds
+        gameBodyUl.appendChild(wordLi)
+      }
+      else {
+        //go to score page 
+        //patch score here?
+        //at the point where the timer ends
+        //how do i get access to the scoreCount so that i can set the end scoreCount
+        //to the games new score value?
+        
+        clearInterval(countDown)
+        checkForZero()
+        return -1;
+      }
+    }, 1000)
+}
+
+
+//refreshes page lol
+function listenLogout() {
+  const logoutBtn = document.getElementById('logout')
+  logoutBtn.addEventListener('click', function (event) {
+    if (event.target.id === 'logout-link') {
+      document.location.reload(true)
+    }
   })
 }
 
-//gets words to add to a list at interval time
-
-function displayTheWords(words){
-  
+//taking input from user and removing a word from main body of game
+//score is incremented here
+function listenForUserInput() {
   const gameBodyUl = document.getElementById('game-ul')
-  let list = gameBodyUl.children
-  let arr = [...list]
-  // if(arr.length < 20){
-
-  
-
-     setInterval(function() {
-      const wordLi = document.createElement('li')
-      //getting a random word obj
-      let randomWord = words[Math.floor(Math.random() * words.length)]
-      //setting our created wordLi id 
-      wordLi.id = randomWord.id
-      wordLi.value = randomWord.letter_count
-      //setting our created wordLi text
-      wordLi.innerText = randomWord.title
-      //append in an interval of 2.5seconds
-      gameBodyUl.appendChild(wordLi)    
-    }, 2500)
-  // } else {
-
-  // }
-
-  }
-
-  
-  //refreshes page lol
-  function listenLogout() {
-    const logoutBtn = document.getElementById('logout')
-    logoutBtn.addEventListener('click', function(event) {
-      if(event.target.id === 'logout-link'){
-        document.location.reload(true)
+  const userInput = document.getElementById('input-word')
+  const scoreDiv = document.getElementById('score')
+  userInput.addEventListener('submit', (event) => {
+    event.preventDefault()
+    let inputTarget = event.target[0]
+    let scoreCount = parseInt(scoreDiv.innerText)
+    let list = gameBodyUl.children
+    let arr = [...list]
+    arr.forEach(el => {
+      if (inputTarget.value === el.innerText) {
+        let scoreVal = event.target[0].value.length
+        el.remove()
+        scoreCount += scoreVal
+        scoreDiv.innerText = scoreCount
+        //this is what we will patch to game score
       }
     })
-  }
-  
-  //taking input from user and removing a word from main body of game
-  function listenForUserInput() {
-    const gameBodyUl = document.getElementById('game-ul')
-    const userInput = document.getElementById('input-word')
-    const scoreDiv = document.getElementById('score')
-    userInput.addEventListener('submit', (event) => {
-      event.preventDefault()
-      let inputTarget = event.target[0]
-      let scoreCount = parseInt(scoreDiv.innerText)
-      let list = gameBodyUl.children
-      let arr = [...list]
-      arr.forEach(el => {
-        if(inputTarget.value === el.innerText ){
-          console.log(event.target[0].value.length)
-          el.remove()
-          scoreCount += event.target[0].value.length
-          scoreDiv.innerText = scoreCount
-          // ++scoreCount
-          // console.log(scoreCount)
-           }
-      })
-      inputTarget.value = ""
-    })
-  }
+    inputTarget.value = ""
+  })
+}
+
+function checkForZero() {
+  const scoreDiv = document.getElementById('score')
+  let timer = document.getElementById('timer')
+  let timerNum = parseInt(timer.innerText)
+  console.log(parseInt(scoreDiv.innerText))
+  console.log(timer.innerText)
+  const gameId = document.getElementsByClassName('user-scores-tab')[0].id
+  console.log(gameId)
+
+  // fetch(`http://localhost:3000/games/${gameId}`, {
+  //       method: "PATCH",
+  //       headers: { 'Content-Type': 'application/json' },
+  //       body: JSON.stringify({
+  //         player_id: parseInt(scoreDiv.innerText)
+  //       })
+  //     }).then(resp => resp.json()).then(gameData => console.log(gameData))
+
+
+  // mainContainer.innerHTML = `<h1>Your score was: ${scoreDiv.innerText}</h1>`
+
+}
+
+
+
+
+
